dd1ab163b44b5331bd4c269f24deafacd6d2cf75
Demir Dereli <workdemirdereli@gmail.com>
2021-10-06 20:53:49 +0300

    small unimportant changes

--- stanfordApp2/stanfordApp2/ViewController.swift
+++ stanfordApp2/stanfordApp2/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +18,21 @@ class ViewController: UIViewController {
 
 }
 
+class myView: UIView{
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder){
+        fatalError("lol")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else {return}
+        
+        let rectangle1 = CGRect(x: 0, y: 0, width: 200, height: 200)
+        context.setFillColor(UIColor.systemRed.cgColor)
+        context.fill(rectangle1)
+    }
+    
+}
