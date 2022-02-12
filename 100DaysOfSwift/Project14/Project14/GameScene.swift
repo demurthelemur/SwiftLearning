140b1e1dd52e8c146502c38a5046b60c682c932e
Demir Dereli <workdemirdereli@gmail.com>
2022-02-12 18:03:01 +0300

    Whack-a-Penguin game Done!

--- 100DaysOfSwift/Project14/Project14/GameScene.swift
+++ 100DaysOfSwift/Project14/Project14/GameScene.swift
@@ -10,6 +10,10 @@ import SpriteKit
 class GameScene: SKScene {
     var slots = [WhackSlot]()
     var gameScore: SKLabelNode!
+    var scoreEndScreen: SKLabelNode!
+    
+    var popupTime = 0.85
+    var numRounds = 0
     
     var score = 0{
         didSet{
@@ -35,11 +39,44 @@ class GameScene: SKScene {
         for i in 0 ..< 4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320)) }
         for i in 0 ..< 5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
         for i in 0 ..< 4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140)) }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1){
+            [weak self] in
+            self?.createEnemy()
+        }
 
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
         
+        for node in tappedNodes{
+            guard let whackSlot = node.parent?.parent as? WhackSlot else {continue}
+            whackSlot.charNode.xScale = 1
+            whackSlot.charNode.yScale = 1
+            if node.name == "charFriend"{
+                if !whackSlot.isVisible {continue}
+                if whackSlot.isHit {continue}
+                
+                whackSlot.hit()
+                score -= 5
+                
+                run(SKAction.playSoundFileNamed("whackBad.caf", waitForCompletion: false))
+            } else if node.name == "charEnemy"{
+                if !whackSlot.isVisible {continue}
+                if whackSlot.isHit {continue}
+                
+                whackSlot.charNode.xScale = 0.85
+                whackSlot.charNode.yScale = 0.85
+                
+                whackSlot.hit()
+                score += 1
+                
+                run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+            }
+        }
     }
     
     func createSlot(at position: CGPoint){
@@ -47,5 +84,45 @@ class GameScene: SKScene {
         slot.configure(at: position)
         addChild(slot)
         slots.append(slot)
+    }
+    
+    func createEnemy(){
+        numRounds += 1
+        
+        if numRounds >= 30{
+            for slot in slots{
+                slot.hide()
+            }
+            let gameOver = SKSpriteNode(imageNamed: "gameOver")
+            gameOver.position = CGPoint(x: 500, y: 430)
+            gameOver.zPosition = 1
+            addChild(gameOver)
+            scoreEndScreen = SKLabelNode(fontNamed: "Chalkduster")
+            scoreEndScreen.text = "Score: \(score)"
+            scoreEndScreen.position = CGPoint(x:508, y:330)
+            scoreEndScreen.fontSize = 70
+            scoreEndScreen.zPosition = 1
+            addChild(scoreEndScreen)
+            run(SKAction.playSoundFileNamed("gameOver.caf", waitForCompletion: false))
+            return
+        }
+        popupTime *= 0.991
+        
+        slots.shuffle()
+        slots[0].show(hideTime: popupTime)
+        
+        if Int.random(in: 0...12) > 4 {slots[1].show(hideTime: popupTime)}
+        if Int.random(in: 0...12) > 8 {slots[2].show(hideTime: popupTime)}
+        if Int.random(in: 0...12) > 10 {slots[3].show(hideTime: popupTime)}
+        if Int.random(in: 0...12) > 11 {slots[4].show(hideTime: popupTime)}
+        
+        let minDelay = popupTime / 2.0
+        let maxDelay = popupTime * 2
+        let delay = Double.random(in: minDelay...maxDelay)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay){
+            [weak self] in
+            self?.createEnemy()
+        }
     }
 }
--- 100DaysOfSwift/Project14/Project14/WhackSlot.swift
+++ 100DaysOfSwift/Project14/Project14/WhackSlot.swift
@@ -10,6 +10,9 @@ import SpriteKit
 class WhackSlot: SKNode {
     var charNode: SKSpriteNode!
     
+    var isVisible = false
+    var isHit = false
+    
     func configure (at position: CGPoint){
         self.position = position
         
@@ -27,5 +30,44 @@ class WhackSlot: SKNode {
         cropNode.addChild(charNode)
         
         addChild(cropNode)
+    }
+    
+    func show(hideTime: Double){
+        if isVisible {return}
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        isVisible = true
+        isHit = false
+        
+        if Int.random(in: 0...2) == 0{
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else{
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + hideTime * 3.5){
+            [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible { return }
+
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
+    }
+    
+    func hit(){
+        isHit = true
+        
+        let delay = SKAction.wait(forDuration: 0.25)
+        let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
+        let notVisible = SKAction.run{ [weak self] in self?.isVisible = false}
+        
+        let sequence = SKAction.sequence([delay, hide ,notVisible])
+        charNode.run(sequence)
     }
 }
